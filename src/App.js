6ccfa8e20883763926cf16614532dfa776f32283
Blackline99 <denismirkotan@gmail.com>
2021-01-09 15:14:44 +0200

    git push\

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import './components/Header.css';
+import './components/Profile.css';
+import './components/Navbar.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
